6f1eba200a8b7d5a608247ac0a629ab4027f3815
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-02-13 21:51:26 +0530

    Add files via upload

---
+++ C_Language/loop programs in c/star3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+    int i,t ;
+
+    for(i=1;i<=5;i++){  
+
+          printf("\n");
+ 
+    for(t=i;t<=i;t++){
+
+        printf("\t");
+
+        for(i=t;t<=i;t++){
+
+         printf("*");
+
+        }
+
+
+    }
+         
+
+    }
+   return 0;
+
+}
