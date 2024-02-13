bd01966d7128d95d944e565079950dc890fa8003
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-02-13 18:06:32 +0530

    Add files via upload

---
+++ C_Language/practice/star2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ 
+  printf("\t*\n");
+    printf("\t\t*\n");
+
+  printf("\t\t\t*\n");
+
+  printf("\t\t\t\t*\n");
+
+  printf("\t\t\t\t\t*\n");
+
+ // five time star without loop
+ return 0;
+}
---
+++ C_Language/practice/star3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ 
+  printf("*\n");
+    printf("**\n");
+
+  printf("***\n");
+
+  printf("****\n");
+
+  printf("*****\n");
+
+ // five time star without loop
+ return 0;
+}
