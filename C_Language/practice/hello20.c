c052c77fef22afec0b50a90fa70f7c8d282017ce
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-02-13 18:07:36 +0530

    Add files via upload

---
+++ C_Language/practice/hello20.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n"); 
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+  printf("hello word\n");
+ 
+
+    // hello word without loop 20 time
+    return 0;
+  
+
+}
