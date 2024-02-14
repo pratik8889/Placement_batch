a7eb4de18b41116f53eff2a1faec2887fc2e8730
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-02-14 09:26:26 +0530

    Add files via upload

---
+++ C_Language/loop programs in c/1to10odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  
+   int i;
+
+   printf("odd  number bettwen 1 to 10:");
+
+   for(i=1;i<=10;i++){
+
+       if(i%2!=0){
+        printf("\n%d\n",i);
+       }
+      
+   }
+
+
+}
