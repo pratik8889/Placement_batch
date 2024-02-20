6a230a132cec14850f49ebdd993ead79eae70fac
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-02-20 09:11:40 +0530

    Add files via upload

---
+++ C_Language/Pattern matching/2table.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+    int  num = 2 ;
+    int i;
+       printf("print table of 2:\n");
+          
+      for(i=1 ; i<=10 ; i++)
+         {
+ 
+         printf("%d*%d=%d\n",num,i,num*i);
+
+
+
+         }
+
+         return 0; 
+    
+
+
+
+}
