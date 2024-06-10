d5cf748ccbb8b0ba058db01b009f071352d29b5b
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-06-10 14:35:46 +0530

    Add files via upload

---
+++ C++_Language/C++_Programs/11_typecast.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+ 
+ int main () 
+              {
+                float a=50.55 ;
+                  
+                   cout<<"the value of a is : "<<(int)a<<endl; // we chnaged value float to int using typecast
+            
+             return 0 ; 
+              }
---
+++ C++_Language/C++_Programs/12_const_variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+ 
+
+  int main() {
+
+               const float pie = 3.14; // value of constant variable is fixed it cant be changed in program
+
+               cout<<"value of pie is:"<<pie<<endl;
+
+               return 0;
+
+
+  }
