d90db34d6748902f3391894f59de06181d6ab33e
pratik8889 <157903255+pratik8889@users.noreply.github.com>
2024-06-28 16:19:29 +0530

    Create Class1.java

---
+++ Java/Class_&_Object/Class1.java
@@ -0,0 +1,18 @@
+public class Class1 {
+
+	   void method1() {
+		   System.out.println("inside method 1");
+	   }
+	   
+	   void method2() {
+		   System.out.println("inside method 2");
+	   }
+	 
+	   public static void main(String[] args) {
+		   
+		   Class1 obj1 = new Class1();
+		   obj1.method1();
+		   obj1.method2();
+	   }
+	 
+}
